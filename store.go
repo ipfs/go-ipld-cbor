252690b7866985fb94dee5a1852d672dbb8ee910
whyrusleeping <why@ipfs.io>
2020-02-04 13:45:05 -0800

    add helper constructor for inmem cbor store

--- store.go
+++ store.go
@@ -152,3 +152,28 @@ func (se SerializationError) Is(o error) bool {
 	_, ok := o.(*SerializationError)
 	return ok
 }
+
+func NewMemCborStore() IpldStore {
+	return NewCborStore(newMockBlocks())
+}
+
+type mockBlocks struct {
+	data map[cid.Cid]block.Block
+}
+
+func newMockBlocks() *mockBlocks {
+	return &mockBlocks{make(map[cid.Cid]block.Block)}
+}
+
+func (mb *mockBlocks) Get(c cid.Cid) (block.Block, error) {
+	d, ok := mb.data[c]
+	if ok {
+		return d, nil
+	}
+	return nil, fmt.Errorf("Not Found")
+}
+
+func (mb *mockBlocks) Put(b block.Block) error {
+	mb.data[b.Cid()] = b
+	return nil
+}
